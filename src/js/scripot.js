1737e463264bf7a55703631752703c459e895575
lucaspenteadosouza07@gmail.com <lucaspenteadosouza07@gmail.com>
2025-07-28 18:37:23 -0300

    Portifólio Online

---
+++ src/js/scripot.js
@@ -0,0 +1,30 @@
+const header = document.querySelector ("header");
+
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 0)
+});
+
+let menu = document.querySelector('#menu-icon');
+let navList = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navList.classList.toggle('active');
+};
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navList.classList.remove('active');
+    documentBy
+}
+
+const sr = ScrollReveal ({
+    distance: '45px',
+    duration: '2700',
+    reset: true
+})
+
+sr.reveal('.home-text',{delay:150, origin:'left'})
+sr.reveal('.home-img',{delay:150, origin:'right'})
+
+sr.reveal('.sub-service,.about,.portifolio,.contact',{delay:50, origin:'bottom'})
